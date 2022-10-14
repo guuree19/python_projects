5e43d757f04c11e92029e9fcbcc75049fe1457c1
Abdulaziz Ali <guuree1982@gmail.com>
2022-10-14 00:26:18 -0400

    first commit

---
+++ functions_practice.py
@@ -0,0 +1,19 @@
+def hello():
+    print("hello my dear john!")
+
+hello()
+
+
+
+def pack(a,b,c):
+    return a, b, c
+    
+print(pack(1,2,3))
+
+
+
+def eat_lunch():
+    list = []
+    print("My lunchbox is empty!")
+
+eat_lunch()
